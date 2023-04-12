e04eee7d6c1b4a17c32038c1598ee8be7d038754
bohuslava-p <127511202+bohuslava-p@users.noreply.github.com>
2023-04-11 18:07:18 -0700

    Change to a five-round game and print the result

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-// player makes choice (through input) * case insensitive
-let playersChoice = prompt("Please enter your choice for game", "")
-playersChoice = playersChoice.toLowerCase();
-// computer makes it's choice by random
-// generate random number from 0 to 2
-let randomIndex = Math.floor(Math.random() * 3);
-// pick word from an array with random index
-let choices = ["rock", "paper", "scissors"];
-let computersChoice = choices[randomIndex];
-
 let playerScore = 0;
 let computerScore = 0;
 
+let playersChoice;
+let computersChoice;
 function gameRound() {
+
+    // player makes choice (through input) * case insensitive
+    playersChoice = prompt("Please enter your choice for game", "")
+    playersChoice = playersChoice.toLowerCase();
+
+    // computer makes it's choice by random
+    // generate random number from 0 to 2
+    let randomIndex = Math.floor(Math.random() * 3);
+    // pick word from an array with random index
+    let choices = ["rock", "paper", "scissors"];
+    computersChoice = choices[randomIndex];
+
     // if player chose rock:
     if (playersChoice === "rock") {
         // switch for computer's choice:  
         switch (computersChoice) {
             // case rock = a tie
             case "rock":
-                return "It's a tie! Play next round";
+                return "It's a tie!";
                 break;
             // case paper = computer won
             case "paper":
@@ -32,14 +36,13 @@ function gameRound() {
                 break;
         }
     }
-
     // if player chose paper:
-    if (playersChoice === "paper") {
+    else if (playersChoice === "paper") {
         // switch for computer's choice: 
         switch (computersChoice) {
             // case paper = a tie
             case "paper":
-                return "It's a tie! Play next round";
+                return "It's a tie!";
                 break;
             // case scissors = computer won
             case "scissors":
@@ -55,12 +58,12 @@ function gameRound() {
     }
 
     // if player chose scissors:
-    if (playersChoice === "scissors") {
+    else if (playersChoice === "scissors") {
         // switch for computer's choice:
         switch (computersChoice) {
             // case scissors = a tie
             case "scissors":
-                return "It's a tie! Play next round";
+                return "It's a tie!";
                 break;
             // case rock = computer won
             case "rock":
@@ -73,17 +76,33 @@ function gameRound() {
                 return "You won! Scissors cuts Paper";
                 break;
         }
+    }  
+    else {
+        alert("Incorrect word. Choose 'Rock', 'Paper' or 'Scissors'");
+        gameRound(playersChoice, computersChoice);
     }
-
+ 
 }
-console.log("player " + playersChoice);
-console.log("Comp " + computersChoice);
 
-console.log(gameRound());
-console.log(computerScore);
-console.log(playerScore);
+function playFiveRounds(){
+    for (let i=1; i<=5; i++){
+        alert(gameRound(playersChoice, computersChoice));
+    }
+}
 
+function printResult(){
+    if (playerScore > computerScore) {
+        return `You won! Your score: ${playerScore}, computer's score: ${computerScore}`;
+    }
+    else if (playerScore == computerScore){
+        alert (`It's a tie! Your score: ${playerScore}, computer's score: ${computerScore}. Play next round to break the tie!`);
+        gameRound();
+        return printResult();
+    }    
+    else {
+        return `You lost! Your score: ${playerScore}, computer's score: ${computerScore}`;
+    }
+}
 
-// print out the result
-
-
+playFiveRounds();
+alert(printResult());
